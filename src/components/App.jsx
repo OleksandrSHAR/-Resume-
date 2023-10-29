b82341a6b7f67d5d9a38ac5de7ac9d8b002d30bb
OleksandrSHAR <125485600+OleksandrSHAR@users.noreply.github.com>
2023-10-29 22:37:56 +0200

    first

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,245 @@
+import {
+  Wrapper,
+  MainContent,
+  MainContentTitel,
+  MyProfession,
+  MyName,
+  MyDescr,
+  AboutMeConteiner,
+  ProjectItem,
+  ProjectLst,
+  ProjectConteiner,
+  ProjectItemDescr,
+  ProjectItemLink,
+  EducationList,
+  EducationItem,
+  WorkExperienceContainer,
+  WorkExperiencePosition,
+  WorkExperiencePres,
+  WorkExperienceCompani,
+  WorkPeriod,
+  SidearTitel,
+  MyPhoto,
+  Sidear,
+  SidearCont,
+  ContactsType,
+  Contacts,
+  SkillsList,
+  SkillsListI,
+  SkillsListT,
+} from './App.style';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <head>
+        <title>My resume</title>
+      </head>
+      <body>
+        {/* Wrapper   */}
+        <Wrapper>
+          {/* Main content  */}
+
+          <MainContent>
+            {/* About me  */}
+            <AboutMeConteiner>
+              <MyProfession>Full-stack developer</MyProfession>
+              <MyName>Sharyak Oleksandr</MyName>
+
+              <MyDescr>
+                Hello, I am a non-conflictual and calm person, like to learn
+                something new and develop, believe that you should not stop on
+                the way to the goal, even if everything does not go as you
+                originally planned.
+              </MyDescr>
+            </AboutMeConteiner>
+
+            {/* My Projects  */}
+            <ProjectConteiner>
+              <MainContentTitel>Projects</MainContentTitel>
+              <ProjectLst>
+                <ProjectItem>
+                  <p>Team project</p>
+                  <ProjectItemLink
+                    href="https://github.com/Kotiko-UA/team17-we-can-do-it-project"
+                    target="_blank"
+                  >
+                    Link to GitHub
+                  </ProjectItemLink>
+                  <ProjectItemDescr>
+                    A shop of watches. Roule:developer. [ JavaScript , CSS ,
+                    HTML]
+                  </ProjectItemDescr>
+                </ProjectItem>
+                <ProjectItem>
+                  <p>Solo project</p>
+                  <ProjectItemLink
+                    href="https://github.com/OleksandrSHAR/goit-react-hw-05-movies"
+                    target="_blank"
+                  >
+                    Link to GitHub
+                  </ProjectItemLink>
+                  <ProjectItemDescr>
+                    Movie site. Roule:developer. [React , HTML]
+                  </ProjectItemDescr>
+                </ProjectItem>
+                <ProjectItem>
+                  <p>Solo project</p>
+                  <ProjectItemLink
+                    href="https://github.com/OleksandrSHAR/goit-react-hw-08-phonebook"
+                    target="_blank"
+                  >
+                    Link to GitHub
+                  </ProjectItemLink>
+                  <ProjectItemDescr>
+                    Phonebook.Roule:developer. [ React.js, Node.js ]
+                  </ProjectItemDescr>
+                </ProjectItem>
+              </ProjectLst>
+            </ProjectConteiner>
+
+            {/* Work experience  */}
+            <WorkExperienceContainer>
+              <h3>Work Experience</h3>
+              {/* Company 1   */}
+              <WorkExperiencePres>
+                <WorkExperienceCompani>
+                  Zorya-Mashproekt / Electrician
+                </WorkExperienceCompani>
+
+                <WorkPeriod>September/2020 - October/2021</WorkPeriod>
+                <ul class="work-duties-list">
+                  <WorkExperiencePosition>
+                    Maintenance and repair of electrical appliances
+                  </WorkExperiencePosition>
+                  <WorkExperiencePosition>
+                    Strict adherence to safety rules
+                  </WorkExperiencePosition>
+                </ul>
+              </WorkExperiencePres>
+              {/* Company 2   */}
+              <WorkExperiencePres>
+                <WorkExperienceCompani>
+                  Armed Forces of Ukraine / Military
+                </WorkExperienceCompani>
+                <WorkPeriod>October/2021</WorkPeriod>
+                <ul class="work-duties-list">
+                  <WorkExperiencePosition>Discipline</WorkExperiencePosition>
+                  <WorkExperiencePosition>
+                    Fulfillment of assigned tasks
+                  </WorkExperiencePosition>
+                  <WorkExperiencePosition>Adaptation</WorkExperiencePosition>
+                </ul>
+              </WorkExperiencePres>
+            </WorkExperienceContainer>
+            {/* Education   */}
+            <div>
+              <h3>Education</h3>
+              <EducationList>IT School GoIT </EducationList>
+              <EducationItem>Fullstack Developer 2023 - 2023</EducationItem>
+              <EducationList>
+                Nikolaev Professional Engineering Lyceum
+              </EducationList>
+              <EducationItem>
+                Electrician for repair and maintenance of electrical equipment
+                2018 - 2020
+              </EducationItem>
+            </div>
+          </MainContent>
+          {/* Sidebar  */}
+          <Sidear>
+            <MyPhoto
+              width={370}
+              height={400}
+              src={require('./photo/photo1.png')}
+            />
+            {/* Contacts   */}
+            <SidearCont>
+              <SidearTitel>Contacts:</SidearTitel>
+              <p class="contats-item">
+                <ContactsType>Mobile:</ContactsType>
+                <Contacts href="tel:+388888888888" class="contacts-number">
+                  +38 0953566694
+                </Contacts>
+              </p>
+              <p class="contats-item">
+                <ContactsType>Mail:</ContactsType>
+                <Contacts
+                  href="mailto:t.goitinskiy@gmail.com"
+                  class="contacts-email"
+                >
+                  oleksandrsharyak@gmail.com
+                </Contacts>
+              </p>
+              <p class="contats-item">
+                <ContactsType>Git:</ContactsType>
+                <Contacts
+                  href="https://github.com/OleksandrSHAR"
+                  target="_blank"
+                >
+                  GitHub
+                </Contacts>
+              </p>
+              <p class="contats-item">
+                <ContactsType>LinkedIn:</ContactsType>
+                <Contacts
+                  href="https://www.linkedin.com/in/oleksandr-sharyak-b72a47289/"
+                  target="_blank"
+                >
+                  In
+                </Contacts>
+              </p>
+            </SidearCont>
+
+            {/* Tech skills  */}
+            <SidearCont>
+              <SidearTitel>Tech Skills</SidearTitel>
+              <SkillsList>
+                <SkillsListI>
+                  {' '}
+                  <SkillsListT>HTML5</SkillsListT>{' '}
+                </SkillsListI>
+                <SkillsListI>
+                  {' '}
+                  <SkillsListT>CSS3</SkillsListT>{' '}
+                </SkillsListI>
+                <SkillsListI>
+                  {' '}
+                  <SkillsListT>GIT</SkillsListT>{' '}
+                </SkillsListI>
+
+                <SkillsListI>
+                  {' '}
+                  <SkillsListT>JavaScript</SkillsListT>{' '}
+                </SkillsListI>
+                <SkillsListI>
+                  {' '}
+                  <SkillsListT>React.js</SkillsListT>{' '}
+                </SkillsListI>
+              </SkillsList>
+            </SidearCont>
+
+            {/* Soft skills  */}
+            <SidearCont>
+              <h3 class="sidear-titel">Soft Skills</h3>
+              <SkillsList>
+                <SkillsListI>
+                  {' '}
+                  <SkillsListT>listening skills</SkillsListT>{' '}
+                </SkillsListI>
+                <SkillsListI>
+                  {' '}
+                  <SkillsListT>Adapting to situation</SkillsListT>{' '}
+                </SkillsListI>
+                <SkillsListI>
+                  {' '}
+                  <SkillsListT>Teamwork</SkillsListT>{' '}
+                </SkillsListI>
+              </SkillsList>
+            </SidearCont>
+          </Sidear>
+        </Wrapper>
+      </body>
     </div>
   );
 };
---
+++ src/components/App.style.jsx
@@ -0,0 +1,180 @@
+import { styled } from 'styled-components';
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: row-reverse;
+  background-color: #fff;
+  width: 1200px;
+  margin: 0 auto;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+`;
+export const MainContent = styled.div`
+  padding: 88px;
+`;
+export const MainContentTitel = styled.h3`
+  font-style: normal;
+  font-weight: 700;
+  font-size: 22px;
+  line-height: 27px;
+
+  color: #000000;
+  margin: 0 0 8px 0;
+`;
+export const MyProfession = styled.h4`
+  font-style: normal;
+  font-weight: 700;
+  font-size: 14px;
+  line-height: 17px;
+
+  color: #000000;
+  margin: 0 0 18px 0;
+`;
+export const MyName = styled.h1`
+  font-style: normal;
+  font-weight: 700;
+  font-size: 45px;
+  line-height: 55px;
+
+  color: #000000;
+  margin: 0 0 28px 0;
+`;
+export const MyDescr = styled.p`
+  font-style: normal;
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 24px;
+
+  color: #595959;
+  width: 525px;
+`;
+export const AboutMeConteiner = styled.div`
+  margin-bottom: 64px;
+`;
+export const ProjectItem = styled.li`
+  font-style: normal;
+  font-weight: 700;
+  font-size: 14px;
+  line-height: 24px;
+
+  color: #000000;
+`;
+export const ProjectLst = styled.ol`
+  padding: 0 0 0 16px;
+  margin: 0;
+`;
+export const ProjectConteiner = styled.div`
+  margin-bottom: 48px;
+`;
+export const ProjectItemDescr = styled.span`
+  font-style: normal;
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 24px;
+
+  color: #595959;
+`;
+export const ProjectItemLink = styled.a`
+  font-style: normal;
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 24px;
+
+  text-decoration-line: underline;
+
+  color: #005fff;
+`;
+export const EducationList = styled.p`
+  font-style: normal;
+  font-weight: 700;
+  font-size: 16px;
+  line-height: 20px;
+
+  color: #6d9bf5;
+`;
+export const EducationItem = styled.p`
+  font-style: normal;
+  font-weight: 700;
+  font-size: 14px;
+  line-height: 17px;
+
+  color: #000000;
+`;
+export const WorkExperienceContainer = styled.div`
+  margin-bottom: 44px;
+`;
+export const WorkExperiencePosition = styled.li`
+  font-style: normal;
+  font-weight: 700;
+  font-size: 16px;
+  line-height: 20px;
+
+  margin: 0;
+
+  color: #000000;
+`;
+export const WorkExperiencePres = styled.div`
+  margin: 16px 0 8px 0;
+`;
+export const WorkExperienceCompani = styled.h4`
+  color: #6d9bf5;
+`;
+export const WorkPeriod = styled.p`
+  font-style: normal;
+  font-weight: 400;
+  font-size: 12px;
+  line-height: 15px;
+
+  color: #a8a8a8;
+  margin: 12px 0 8px 0;
+`;
+export const MyPhoto = styled.img`
+  margin-top: 40px;
+`;
+export const SidearTitel = styled.h3`
+  font-style: normal;
+  font-weight: 700;
+  font-size: 22px;
+  line-height: 27px;
+
+  color: #161d2a;
+  margin-bottom: 8px;
+`;
+export const Sidear = styled.aside`
+  width: 370px;
+  background: #e0ebfe;
+`;
+export const SidearCont = styled.div`
+  margin: 40px 0 0 40px;
+`;
+export const ContactsType = styled.span`
+  font-style: normal;
+  font-weight: 700;
+  font-size: 14px;
+  line-height: 24px;
+
+  color: #161d2a;
+  margin-right: 8px;
+`;
+export const Contacts = styled.a`
+  font-style: normal;
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 24px;
+
+  color: #2979ff;
+`;
+export const SkillsList = styled.ul`
+  margin: 0;
+  padding: 0 0 0 20px;
+`;
+export const SkillsListI = styled.li`
+  color: #6d9bf5;
+`;
+export const SkillsListT = styled.span`
+  font-style: normal;
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 24px;
+
+  color: #161d2a;
+`;
